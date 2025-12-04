cf01f29def7a11f230f5a05522669fdc7658dc0d
cshireman <chris@shireman.net>
2025-12-04 08:20:04 -0800

    Initial Commit

---
+++ ChrisUI/ChrisUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChrisUIApp.swift
+//  ChrisUI
+//
+//  Created by Chris Shireman on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ChrisUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChrisUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChrisUI
+//
+//  Created by Chris Shireman on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
